3e6da884ea07012ec1db5c55afc383e04fa952bd
FalseHuman <emilkhazioff@yandex.com>
2020-12-11 19:32:44 +0500

    realese v1

--- frontend/src/hooks/auth.hook.js
+++ frontend/src/hooks/auth.hook.js
@@ -4,7 +4,7 @@ const storageName = 'userData'
 
 export const useAuth = () => {
   const [token, setToken] = useState(null)
-  //const [ready, setReady] = useState(false)
+  const [ready, setReady] = useState(false)
   const [userId, setUserId] = useState(null)
 
   const login = useCallback((jwtToken, id) => {
@@ -29,8 +29,9 @@ export const useAuth = () => {
     if (data && data.token) {
       login(data.token, data.userId)
     }
+    setReady(true)
   }, [login])
 
 
-  return { login, logout, token, userId}//, ready }
+  return { login, logout, token, userId, ready }
 }
--- frontend/src/pages/LinksPage.js
+++ frontend/src/pages/LinksPage.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useCallback, useContext, useEffect, useState } from 'react';
+import {useHttp} from '../hooks/http.hook';
+import {Loader} from '../components/Loader';
+import {LinksList} from '../pages/LinksList'
+import { AuthContext } from '../context/AuthContext';
 
 export const LinksPage = () =>{
+    const [links, setLinks] = useState([])
+    const {loading, request} = useHttp()
+    const {token} = useContext(AuthContext)
+
+    const fetchLinks = useCallback( async () => {
+        try {
+            const fetched = await request('/api/link', 'GET', null, {
+                Authorization: `Bearer ${token}`
+            })
+            setLinks(fetched)
+        } catch (e) {}
+    }, [token, request])
+    useEffect(() => {
+        fetchLinks()
+    }, [fetchLinks])
+
+    if (loading) {
+        return <Loader />
+      }
+    
     return (
-        <div>
-            <h1>LinksPage</h1>
-        </div>
+        <>
+        {!loading && <LinksList  links={links}/>}
+        </>
     )
 }
